adf47ec01835a6937ef71293c9457019c7247f7c
Azael-Ruh <42300430+Azael-Ruh@users.noreply.github.com>
2018-08-13 11:45:56 +0200

    Update items.lua

--- data/items.lua
+++ data/items.lua
@@ -4,7 +4,7 @@ data:extend({
       name = "tnt",
       icon = "__Enhanced-industrial-techniques__/graphics/items/tnt.png",
       icon_size = 32,
-      stack_size = 200,
+      stack_size = 100,
       subgroup = "raw-material",
       flags = { "goes-to-main-inventory"},
       order = "m"
