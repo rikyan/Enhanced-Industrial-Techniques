0adc67d681a753ecbd17cd1176b8942b776c67ff
Azael-Ruh <42300430+Azael-Ruh@users.noreply.github.com>
2018-08-18 17:08:06 +0200

    Update technologies.lua

--- data/technologies.lua
+++ data/technologies.lua
@@ -1,27 +1,28 @@
 data:extend({
   {
     type = "technology",
-    name = "water-hydrolysis",
+    name = "water-electrolysis",
+    icon = "__Enhanced-industrial-techniques__/graphics/technology/water-hydrolysis.png",
     icon_size = 128,
-    icon = "__base__/graphics/technology/oil-processing.png",
-    --effects =
-    --{
-    --  {
-    --    type = "unlock-recipe",
-    --    recipe = "water-hydrolysis"
-    --  }
-    --},
+    effects =
+    {
+      {
+        type = "unlock-recipe",
+        recipe = "water-electrolysis"
+      }
+    },
     prerequisites = {"oil-processing"},
     unit =
     {
-      count = 100,
+      count = 150,
       ingredients =
       {
         {"science-pack-1", 1},
         {"science-pack-2", 1}
       },
       time = 30
-    }
+    },
+    order = "d-d"
   },
   {
     type = "technology",
